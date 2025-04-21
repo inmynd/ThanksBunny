e3918a6c9796ab219521ebf38d7ec2573eea8881
Minah Park | Ari <inmynd.ca@gmail.com>
2025-04-22 08:22:09 +0900

    Initial Commit

---
+++ Thanks/ThanksApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThanksApp.swift
+//  Thanks
+//
+//  Created by 박민아 on 4/22/25.
+//
+
+import SwiftUI
+
+@main
+struct ThanksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ThanksTests/ThanksTests.swift
@@ -0,0 +1,17 @@
+//
+//  ThanksTests.swift
+//  ThanksTests
+//
+//  Created by 박민아 on 4/22/25.
+//
+
+import Testing
+@testable import Thanks
+
+struct ThanksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
